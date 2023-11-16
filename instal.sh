d9a7e09b72f5e5a68c12332673f234d1dadf910f
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2023-11-16 22:39:53 +0530

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade
+sudo apt install gi
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo bash -
+apt install nodejs
+npm -v
+git clone https://github.com/MBG-Codes-You/Minactyl.git
+cd Minactyl
+npm install
+npm install pm2 -g
