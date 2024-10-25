66113fb21b910841a2682dad498a6979b521aaf1
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2024-10-25 14:23:38 +0530

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -3,7 +3,7 @@ sudo apt install gi
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo bash -
 apt install nodejs
 npm -v
-git clone https://github.com/MBG-Codes-You/Minactyl.git
+git clone https://github.com/MBG1337/Minactyl.git
 cd Minactyl
 npm install
 npm install pm2 -g
